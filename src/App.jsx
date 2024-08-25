b97d9159fe59751de82ec2ae377d719ba9cb8016
kirdzaza <148518495+kirdzaza@users.noreply.github.com>
2024-08-25 14:22:36 +0700

    test11

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Nav from "./Nav.jsx";
+import Dashboard from "./Dashboard.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Nav />
+      </div>
+      <Dashboard />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Dashboard.jsx
@@ -0,0 +1,15 @@
+// Dashboard.jsx
+import Nav from "./Nav.jsx";
+import Chart from "./Chart.jsx";
+import CarTable from "./CarTable.jsx";
+function Dashboard() {
+  return (
+    <>
+      <Nav />
+      <Chart />
+      <CarTable />
+    </>
+  );
+}
+
+export default Dashboard;
