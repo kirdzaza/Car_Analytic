b22bfa62338242134ec8f7209644fe7765b4f23e
kirdzaza <148518495+kirdzaza@users.noreply.github.com>
2024-08-25 16:57:46 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "./css/index.css";
 import Highlighted from "./Highlighted.jsx";
 import Dashboard from "./Dashboard.jsx";
